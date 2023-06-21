7c5dad3489732e9e43072ef1ed959801eda925fa
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-06-20 22:59:15 -0500

    edit, delete functions working

--- script.js
+++ script.js
@@ -14,5 +14,28 @@ function addNewNote(text = "") {
 <div class="main ${text ? "" : "hidden"}"></div>
 <textarea class="${text ? "hidden" : ""}></textarea>`;
 
+const editBtn = note.querySelector('.edit') 
+const deleteBtn = note.querySelector('.delete') 
+const main = note.querySelector('.main') 
+const textArea = note.querySelector('.textarea') 
+
+textArea.value = text;
+main.innerHTML= marked(text)
+
+deleteBtn.addEventListener('click', () => {
+    note.remove()
+})
+
+editBtn.addEventListener('click', () => {
+    main.classList.toggle('hidden')
+    textArea.classList.toggle('hidden')
+})
+
+textArea.addEventListener('input', (e) => {
+    const { value } = e.target;
+
+    main.innerHTML = marked(value)
+})
+
 document.body.appendChild(note)
 }
